9bd3c1a18fbc6c851a01c5d42c86e2f38f10d402
dierkpiening <60482836+dierkpiening@users.noreply.github.com>
2020-03-19 17:29:25 +0100

    New Funktions
    Suche nach Bundesländern

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace ShareX_Main_Server
         static ITelegramBotClient botClient;
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
             botClient = new TelegramBotClient("1048276961:AAEqjnUyZedMU4N4Vc962K7YqPaVlhRPI2Q");
 
             var me = botClient.GetMeAsync().Result;
@@ -36,10 +39,12 @@ namespace ShareX_Main_Server
         static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             try {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
                 String dbpath = "C:\\CISB.sqlite3";
                 SQLiteConnection connection = new SQLiteConnection("Data Source=" + dbpath);
                 String[] seperator = { " " };
-                Int32 count = 2;
+                Int32 count = 3;
                 String[] cmdlist = e.Message.Text.Split(seperator, count, StringSplitOptions.RemoveEmptyEntries);
                 Console.WriteLine("#### Datenbankverbindung aufgebaut------------{0}");
                 if (e.Message.Text != null || cmdlist[0] != null)
@@ -124,31 +129,62 @@ namespace ShareX_Main_Server
                     }
                     else if (cmdlist[0] == "Ausgangssperre" || cmdlist[0] == "ausgangssperre" || cmdlist[0] == "ausgangssperren")
                     {
+                        if (cmdlist[1] == "bundesland" || cmdlist[1] == "Bundesland" || cmdlist[1] == "bl" || cmdlist[1] == "BL" || cmdlist[1] == "Bl")
+                        {
+                            connection.Open();
+                            SQLiteCommand cmd = new SQLiteCommand("select * from AusgangssperrenLK where upper(BUNDESLAND) = upper('" + cmdlist[2] +"')", connection);
+                            SQLiteDataReader reader = cmd.ExecuteReader();
 
-                        connection.Open();
-                        SQLiteCommand cmd = new SQLiteCommand("select * from AusgangssperrenLK where PLZ = " + cmdlist[1], connection);
-                        SQLiteDataReader reader = cmd.ExecuteReader();
-
-                        if (reader.HasRows)
-                            while (reader.Read())
+                            if (reader.HasRows)
+                                while (reader.Read())
+                                {
+                                    string vLK = reader.GetString(reader.GetOrdinal("LKN"));
+                                    string vBUNDESLAND = reader.GetString(reader.GetOrdinal("BUNDESLAND"));
+                                    string vDatumVON = reader.GetString(reader.GetOrdinal("VON"));
+                                    string vDatumBIS = reader.GetString(reader.GetOrdinal("BIS"));
+                                    string vBEHÖRDE = reader.GetString(reader.GetOrdinal("BEHÖRDE"));
+                                    int vPLZ = reader.GetInt32(reader.GetOrdinal("PLZ"));
+                                    await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Ausgangsperre im Landkreis: " + vLK + " im Bundesland: " + vBUNDESLAND + " verhängt durch: " + vBEHÖRDE + "\n Gültig von:" + vDatumVON + " bis: " + vDatumBIS);
+                                    Console.WriteLine("### Abfrage der Ausgangssperren nach Bundesland data------------{0}");
+                                }
+                            else
                             {
-                                string vLK = reader.GetString(reader.GetOrdinal("LKN"));
-                                string vBUNDESLAND = reader.GetString(reader.GetOrdinal("BUNDESLAND"));
-                                string vDatumVON = reader.GetString(reader.GetOrdinal("VON"));
-                                string vDatumBIS = reader.GetString(reader.GetOrdinal("BIS"));
-                                string vBEHÖRDE = reader.GetString(reader.GetOrdinal("BEHÖRDE"));
-                                int vPLZ = reader.GetInt32(reader.GetOrdinal("PLZ"));
-                                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Ausgangsperre im Landkreis: " + vLK + " im Bundesland: " + vBUNDESLAND + " verhängt durch: " + vBEHÖRDE + "\n Gültig von:" + vDatumVON + " bis: " + vDatumBIS);
-                                Console.WriteLine("### Abfrage der Ausgangssperren data------------{0}");
+                                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Für das Bundesland: " + cmdlist[2] + " ist uns keine Ausgangssperre bekannt!");
+                                Console.WriteLine("### Abfrage der Ausgangssperren nach Bundesland data------------{0}");
                             }
+                            connection.Close();
+
+
+                        }
                         else
                         {
-                            await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Für die PLZ: " + cmdlist[1] + " ist uns keine Ausgangssperre bekannt!");
-                            Console.WriteLine("### Abfrage der Ausgangssperren data------------{0}");
-                        }
-                        connection.Close();
 
+                            connection.Open();
+                            SQLiteCommand cmd = new SQLiteCommand("select * from AusgangssperrenLK where PLZ = " + cmdlist[1], connection);
+                            SQLiteDataReader reader = cmd.ExecuteReader();
+
+                            if (reader.HasRows)
+                                while (reader.Read())
+                                {
+                                    string vLK = reader.GetString(reader.GetOrdinal("LKN"));
+                                    string vBUNDESLAND = reader.GetString(reader.GetOrdinal("BUNDESLAND"));
+                                    string vDatumVON = reader.GetString(reader.GetOrdinal("VON"));
+                                    string vDatumBIS = reader.GetString(reader.GetOrdinal("BIS"));
+                                    string vBEHÖRDE = reader.GetString(reader.GetOrdinal("BEHÖRDE"));
+                                    int vPLZ = reader.GetInt32(reader.GetOrdinal("PLZ"));
+                                    await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Ausgangsperre im Landkreis: " + vLK + " im Bundesland: " + vBUNDESLAND + " verhängt durch: " + vBEHÖRDE + "\n Gültig von:" + vDatumVON + " bis: " + vDatumBIS);
+                                    Console.WriteLine("### Abfrage der Ausgangssperren data------------{0}");
+                                }
+                            else
+                            {
+                                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "Für die PLZ: " + cmdlist[1] + " ist uns keine Ausgangssperre bekannt!");
+                                Console.WriteLine("### Abfrage der Ausgangssperren data------------{0}");
+                            }
+                            connection.Close();
+
+                        }
                     }
+
                     else if (cmdlist[0] == "symptome" || cmdlist[0] == "Symptome" || cmdlist[0] == "erkennen")
                     {
                         Telegram.Bot.Types.Message message = await botClient.SendPhotoAsync(
@@ -168,7 +204,7 @@ namespace ShareX_Main_Server
                          );
                         Console.WriteLine("### Abfrage der SchutzDaten: data------------{0}");
                     }
-                    else if (cmdlist[0] == "hilfe" || cmdlist[0] == "Hilfe" || cmdlist[0] == "h" || cmdlist[0] == "H" )
+                    else if (cmdlist[0] == "hilfe" || cmdlist[0] == "Hilfe" || cmdlist[0] == "h" || cmdlist[0] == "H")
                     {
                         await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "_____ACHTUNG:_____\nSollten sie den verdacht haben an sich mit COVID-19 Infiziert zu haben wenden sie sich an die Bundesweite Ärtzte Hotline 116117.\nBleiben sie zuhause! ");
                     }
@@ -177,13 +213,15 @@ namespace ShareX_Main_Server
                     {
 
 
-                        await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "_____ACHTUNG:_____\nGib info oder Info oder i ein um die Daten zu erhalten.\nUm die aktuellen Zahlen der Bundesländer zu erhalten gebe BL oder bl oder Bundesländer ein\n Ausgangssperre alle oder ausgangssperre alle zeigt dir alle geltenden Ausgangssperren an\n Ausgangssperre 26427 zeigt dir an ob eine Ausgangssperre für deinen PLZ Bereich vorliegt z.B: 26427\n Geben sie tipps oder hyghiene ein um Hinweise zum Schutz vor einer Covid-19 Infektion zu erhalten.\nDurch die eingabe von symptome erhalten sie eine Auflistung der bekannten COVID-19 Symptome");
+                        await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "_____ACHTUNG:_____\nGib info oder Info oder i ein um die Daten zu erhalten.\nUm die aktuellen Zahlen der Bundesländer zu erhalten gebe BL oder bl oder Bundesländer ein\n Ausgangssperre alle oder ausgangssperre alle zeigt dir alle geltenden Ausgangssperren an\n Ausgangssperre 26427 zeigt dir an ob eine Ausgangssperre für deinen PLZ Bereich vorliegt z.B: 26427\nAusgangssperre BL Bayern oder Ausgangssperre Bundesland Bayern zeigt dir die Ausgangssperren der Gemeinden im Land Bayern an\nGeben sie tipps oder hyghiene ein um Hinweise zum Schutz vor einer Covid-19 Infektion zu erhalten.\nDurch die eingabe von symptome erhalten sie eine Auflistung der bekannten COVID-19 Symptome");
                         Console.WriteLine("### Abfrage aber unbekannter Befehl------------{0}");
                     }
                 }
             }
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
                 Console.WriteLine("*-*-*-*-* Ein Fehler ist aufgetreten und wurde abgefangen------------{0}");
 
             }
